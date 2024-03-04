4e412a63e18b6e84a48ceeb3dc94be24c965c419
Mike Sheliga <msheliga1@gmail.com>
2024-03-04 09:30:52 -0500

    db/schema.sql (NO if) and dir structure

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+-- MJS 3.2.24 
+/* There is no ECHO statement in sql! Use SELECT 'String' instead! */ 
+/* I don't think mySQL contains an IF ... THEN type statement. but T-SQL CASE ... and IIF */
+DROP DATABASE IF EXISTS blogpost_db;
+CREATE DATABASE blogpost_db;
+SELECT 'Sucessfully Created blogpost db';
